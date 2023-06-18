8ee41a8dd1a3daf0ee275d17a4d243ec77d364a5
Ricardo <dasilvaricardog@gmail.com>
2023-06-18 10:29:28 -0400

    Mejora en las funciones y fuentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import Navbar from "./components/layout/navbar/Navbar";
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer.jsx";
 
--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -2,11 +2,15 @@ import "./ItemListContainer.css";
 
 const ItemListContainer = ({ greeting, cambiarSaludo }) => {
   //props enviadas desde App, probando enviar con estados (variables y funciones set)
+
   return (
     <div
       className="Saludo"
       onMouseOver={() => {
-        cambiarSaludo("Haz hecho MouseOver!!!");
+        cambiarSaludo("Hola este es otro saludo");
+      }}
+      onMouseOut={() => {
+        cambiarSaludo("Hola Como estas? ");
       }}
     >
       <h1> {greeting}</h1>
