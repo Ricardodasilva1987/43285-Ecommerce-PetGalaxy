a8589e6fb0279aaf6fa07a6b12bfe104d753875c
Ricardo <dasilvaricardog@gmail.com>
2023-06-22 18:39:16 +0200

    Modificacion de navbar y se agregar Listado de productos en js

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import CartWidget from "../../common/cartwidget/cartWidget";
 
 import "./Navbar.css";
@@ -6,19 +7,30 @@ const Navbar = () => {
   return (
     <div className="navbar-main">
       <img
+        className="logo"
         src="https://res.cloudinary.com/drids8rm1/image/upload/v1686842786/Proyecto%20Pet%20Galaxy/Logo_tienda_PET_GALAXY_dcholf.png"
-        alt=""
+        alt="aqui va el logo"
       />
 
-      <ul className="categorias">
-        <li>Todas</li>
-        <li>Alimentos</li>
-        <li>Salud</li>
-        <li>Correas y collares</li>
-        <li>Juguetes</li>
-        <li>Vestuario</li>
-      </ul>
-
+      <Typography>
+        <ul className="categorias">
+          <li>
+            <a href="">Todas</a>
+          </li>
+          <li>
+            <a href="">Alimentos</a>
+          </li>
+          <li>
+            <a href="">Salud</a>
+          </li>
+          <li>
+            <a href="">Correas y collares</a>
+          </li>
+          <li>
+            <a href="">Juguetes</a>
+          </li>
+        </ul>
+      </Typography>
       <div>
         <CartWidget />
       </div>
--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,4 +1,5 @@
 import "./ItemListContainer.css";
+import Typography from "@mui/material/Typography";
 
 const ItemListContainer = ({ greeting, cambiarSaludo }) => {
   //props enviadas desde App, probando enviar con estados (variables y funciones set)
@@ -13,7 +14,9 @@ const ItemListContainer = ({ greeting, cambiarSaludo }) => {
         cambiarSaludo("Hola Como estas? ");
       }}
     >
-      <h1> {greeting}</h1>
+      <Typography variant="h1" sx={{ fontSize: { xs: "3rem", md: "5rem" } }}>
+        {greeting}
+      </Typography>
     </div>
   );
 };
